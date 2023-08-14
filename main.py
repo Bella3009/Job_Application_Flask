726e9efbd9688bf02b328b21ea5f69103e017eb5
Bella3009 <bellasara13@gmail.com>
2023-08-14 10:00:19 +0200

    Retrieving the data inputted in the form

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]  # The name property in the input tag
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        available = request.form["available"]
+        occupation = request.form["occupation"]
+
     return render_template("index.html")
 
 
